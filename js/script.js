66d4024285d56515d2733400b91ed904cc0eb81a
Kos <sinkov.ks@gmail.com>
2020-07-09 21:47:19 +0300

    Added working timer with remaining time to deadline

--- js/script.js
+++ js/script.js
@@ -49,5 +49,65 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // TIMER
+
+    let deadLine = '2020-07-21'; //КОНЕЧНАЯ ДАТА
+
+    // Фун-ция определяюща остаток времени
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()), // переменная с разницей между конечной и текущей датой
+            seconds = Math.floor((t/1000) % 60), //получение остатка секунд
+            mins = Math.floor((t/1000/60) % 60), //получение остатка минут
+            hours = Math.floor(t/(1000 * 60 * 60));
+            // hours = Math.floor((t/1000/60/60) % 24) // количество оставшихся дней
+            // days = Math.floor(t/(1000*60*60*24)) // количество оставшихся дней
+
+        // Вернуть из функции сразу несколько переменных мы просто так не можем
+        return { //Экспортируем объект
+            'total' : t,
+            'hours' : hours,
+            'mins' : mins,
+            'seconds' : seconds
+        };
+    }
+
+    // Ф-ция динамически отсчитывающая время
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'), //док-нт из блока timer.
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        // Ф-ция, которая будет обновлять ф-цию setClock каждую секунду
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+
+            // Ф-ция добавляющая "0", если значение времени меньше 10
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else {
+                    return num;
+                }
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.mins);
+            seconds.textContent = addZero(t.seconds);
+
+            // Условие окончания отчсёта таймера
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadLine); //Вызываем ф-цию с аргументом id #timer
 });
 
